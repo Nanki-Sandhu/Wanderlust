70dc74713b1befac68077862de0b0ec3f53064a4
Nanki-Sandhu <sandhunanki14@gmail.com>
2024-06-18 20:59:47 -0500

    final commit

--- init/data.js
+++ init/data.js
@@ -331,7 +331,7 @@ const sampleListings = [
       filename: "listingimage",
       url: "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGxha2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
     },
-    price: 4000,
+    price: 4000, 
     location: "Aspen",
     country: "United States",
   },
